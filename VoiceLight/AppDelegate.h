687a9acc90048057f16e6b5c25379f99d4947c83
cocoawork <296832852@qq.com>
2017-06-27 08:41:42 +0800

    Initial Commit

---
+++ VoiceLight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VoiceLight
+//
+//  Created by cocoawork on 2017/6/27.
+//  Copyright © 2017年 cocoawork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
